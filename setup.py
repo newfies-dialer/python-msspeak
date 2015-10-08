4ce16e649e23b665ccad3f3d7f0c7cb669579f78
areski <areski@gmail.com>
2015-10-08 11:34:54 +0200

    Update to version v0.1.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='python-msspeak',
-    version='0.1.1',
+    version='0.1.2',
     description="Text-To-Speech with MSSpeak",
     long_description=readme + '\n\n' + history,
     author="Areski Belaid",
