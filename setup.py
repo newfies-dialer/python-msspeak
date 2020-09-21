ed6776ae995702f95289d2d8b623bfe2b7e6a889
areski <areski@gmail.com>
2020-09-21 10:46:04 +0200

    Update to version v0.6.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='python-msspeak',
-    version='0.6.1',
+    version='0.6.2',
     description="Text-To-Speech with MSSpeak",
     long_description=readme + '\n\n' + changelog,
     author="Areski Belaid",
--- update_version.sh
+++ update_version.sh
@@ -1,6 +1,6 @@
 #
 # Usage:
-#   ./update_version.sh 0.6.1
+#   ./update_version.sh 0.6.2
 #
 
 git flow release start v$1
